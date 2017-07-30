ded95f83e66529d8a28f48f56f119dddf244d819
JoseGuzman <sjm.guzman@gmail.com>
2017-07-30 16:44:58 +0200

    future must occur at the beginning

--- inet/__init__.py
+++ inet/__init__.py
@@ -2,6 +2,7 @@
 # Authors: Jose Guzman and Claudia Espinoza
 #=========================================================================
 
+from __future__ import  division
 __version__ = '0.0.1'
 #__author__ = 'Jose Guzman and Claudia Espinoza'
 #__credits__ = 'PVNet'
@@ -9,4 +10,3 @@ __version__ = '0.0.1'
 # directories to load when from PVNet import *
 #__all__ = ['simulation']
 
-from __future__ import  division
