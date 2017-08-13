cb386e5712b9560300880fc6280b74fde4857c00
ClaudiaEsp <claumespinoza@gmail.com>
2017-08-13 14:35:44 +0200

    update TODO and add motifs to import module

--- inet/__init__.py
+++ inet/__init__.py
@@ -10,5 +10,5 @@ __author__ = 'Jose Guzman and Claudia Espinoza'
 __license__ = 'GPL-2.0'
 
 # directories to load when from inet import *
-__all__ = ['utils', 'loader', 'math'] 
+__all__ = ['utils', 'loader', 'math', 'motifs'] 
 
