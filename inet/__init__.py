408a80a76f6f5e6d89f941099441c8f1def987ad
JoseGuzman <sjm.guzman@gmail.com>
2017-08-14 23:46:39 +0200

    bump to version 0.0.6

--- inet/__init__.py
+++ inet/__init__.py
@@ -5,7 +5,7 @@
 # to do from inet import DataLoader
 from inet.loader import DataLoader
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'Jose Guzman and Claudia Espinoza'
 __license__ = 'GPL-2.0'
 
